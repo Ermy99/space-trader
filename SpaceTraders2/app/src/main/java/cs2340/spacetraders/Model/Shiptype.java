3072a28c5707e707bc281ccfdd3d3f8cfaa30862
Aditya Tapshalkar <aditya.taps@gatech.edu>
2019-02-27 12:10:04 -0500

    Added some ships

--- SpaceTraders2/app/src/main/java/cs2340/spacetraders/Model/Shiptype.java
+++ SpaceTraders2/app/src/main/java/cs2340/spacetraders/Model/Shiptype.java
@@ -1,15 +1,30 @@
 package cs2340.spacetraders.Model;
 
 public enum Shiptype {
-    GNAT(10);
+    FLEA("Flea", 10, 20),
+    GNAT("Gnat", 15, 14),
+    FIREFLY("Firefly", 20, 17),
+    MOSQUITO("Mosquito", 15, 13),
+    BUMBLEBEE("Bumblebee", 25, 15),
+    BEETLE("Beetle", 50, 14),
+    HORNET("Hornet", 20, 16),
+    GRASSHOPPER("Grasshopper", 30, 15),
+    TERMITE("Termite", 60, 13),
+    WASP("Wasp", 35, 14);
 
     int cargoSize;
+    int parsecs;
 
-    Shiptype(int cargoSize) {
+    Shiptype(String name, int cargoSize, int parsecs) {
         this.cargoSize = cargoSize;
+        this.parsecs = parsecs;
     }
 
     public int getCargoSize() {
         return cargoSize;
+    }
+    
+    public int getParsecs() {
+        return parsecs;
     }
 }
