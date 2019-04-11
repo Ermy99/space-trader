cf6bec4ecad4925d9c00f292f1398743b3f89347
Ermy99 <eizihirwe@gmail.com>
2019-04-11 00:31:20 -0400

    error fixing

---
+++ SpaceTraders2/app/src/test/java/cs2340/spacetraders/CanTravelTest.java
@@ -0,0 +1,17 @@
+package cs2340.spacetraders;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.*;
+
+import cs2340.spacetraders.Model.Game;
+import cs2340.spacetraders.Model.SolarSystems;
+
+
+public class CanTravelTest {
+    @Test
+    public void checkFuelEmpty() {
+        assertFalse(false);
+    }
+}
